25d6b0394b372beee359b7ead2f0743e9de64f15
Tim Crisp <timothycrisp@gmail.com>
2022-02-06 20:39:15 -0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 /*-------------------------------- Constants --------------------------------*/
-
 const winningCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -10,66 +9,40 @@ const winningCombos = [
 	[0, 4, 8],
 	[2, 4, 6]
 ]
-console.log(winningCombos);
-
-
-// console.log(winningCombos.reduce())
-// 4.1)
-
-
 
 /*---------------------------- Variables (state) ----------------------------*/
 let squares, turn, winner, turnCount, T
-// 1)
 
 /*------------------------ Cached Element References ------------------------*/
 const board = document.querySelector(".board")
-console.log(board); 
-//  X 2.1 
 const gameStatus = document.querySelector("#message")
-console.log(gameStatus)
-//  X 2.2
-
 const gameSquares = document.querySelectorAll(".square")
-console.log(gameSquares);
-const replayDiv = document.getElementById("replay-div");
-console.log(replayDiv);
-
-
+const resetButton = document.getElementById("reset-button");
 
 /*----------------------------- Event Listeners -----------------------------*/
 board.addEventListener("click", handleClick); 
-replayDiv.addEventListener("click", init);
-
-
+resetButton.addEventListener("click", init);
 
 /*-------------------------------- Functions --------------------------------*/
 
 init();
-	// X 3.1
-
 function init() {
-
 	squares = [
 		null, null, null, 
 		null, null, null, 
 		null, null, null];
-	console.log(squares)
-		// X 3.2.1 
-	message = "Player 1 Choose a Square"	
+	message = "X Goes First"
+	resetButton.setAttribute("hidden", true)	
 	turn = 1
 	turnCount = 0
-	// X 3.2.2)
 	winner = null;
-	// X 3.2.3)
-	// **       Winner Variable holds player value (1 or -1),
-	// **       if there's a winner; 'T' if there's a tie.
 	render();
-	// X 3.2.4)
 }
-
 function handleClick(event) {	
 	const index = event.target.id.replace('sq', '')
+	if (winner) {
+		return
+	}
 	if (squares[index] !== null) {
 		return
 	} 
@@ -77,159 +50,48 @@ function handleClick(event) {
 	squares[index] = turn
 	turn *= -1
 	turnCount += 1
-	console.log(turnCount)
-	console.log(squares)
+	resetButton.removeAttribute("hidden")
+	getWinner()
 	render()
 }
-
-
 function render() {
 	gameStatus.textContent = message
 	squares.forEach((square, idx) => {
-		console.log(squares[idx], square, idx);
 		if (square === 1) {
 			gameSquares[idx].textContent = 'X'
-			// gameSquares[idx].style.backgroundColor = 'red';
-			} else if (square === -1) {
+			gameSquares[idx].style.backgroundColor = '#F6D1D9';
+		} else if (square === -1) {
 			gameSquares[idx].textContent = '0'
+			gameSquares[idx].style.backgroundColor = '#F7F0A8';
+		} else if (square === null) {
+			gameSquares[idx].textContent = ''
+			gameSquares[idx].style.backgroundColor = '';
 		}
-		})
-		// if (winner === null) {
-		// 	message = "It's "
-		// }	else if (winner === 1) {
-		// 	console.log('Player One Wins!')
-		// } else if (winner === -1) {
-		// 	console.log('Player Two Wins!')
-		// } else if (winner === 'T') {
-		// 	console.log('The game is a tie!')
-		// }
-
-		getWinner()
-		// messages do not change 
-
-	} 
-	
-	function getWinner(){
-		winningCombos.forEach((combo) => {
-			if (squares[combo[0]]+
-				squares[combo[1]]+
-				squares[combo[2]]=== 3) {
-				gameStatus.textContent = 'X wins!'
+	})
+	if (winner === null) {
+			message = turn === -1 ? "O's Turn" : "X's Turn"
+	}	
+} 
+function getWinner(){
+	winningCombos.forEach((combo) => {
+	if (squares[combo[0]]+
+			squares[combo[1]]+
+			squares[combo[2]]=== 3) {
+			winner = 1	
+			message = 'X wins!'
 			} else if (squares[combo[0]]+
 				squares[combo[1]]+
 				squares[combo[2]] === -3) {
-				gameStatus.textContent = 'O Wins!'
+				winner = -1	
+				message = 'O Wins!'
 			} else if (turnCount === 9) {
-				gameStatus.textContent = "The game is a tie"
+				winner = 'T'
+				message = "This game is a tie!"
 			}
+			resetButton.removeAttribute("hidden")
 		})
-	}
-	
-	// function getWinner(squares, winningCombos) {
-  // 
-	// }
-	
-	// function getWinner() {
-	// 	for (let i = 0; i <= winningCombos.length; i++){
-	// 		winner = getWinner[i]
-	// 	}
-	// } 
-
-	// function checkWinner(){
-	// 	for (let i = 0; i <= 7; i++){
-
-	// 	}
-	// }
-
-/*-----------------------------* Instructions *------------------------------*/
-
-		// ** 3.3.1.2) Style square dependant on the value inside current cell being iterated over (-1, 1, or null)
-	// 3.3.2) Render message on currrent game state:
-		// 3.3.2.1) If winner != null (game still in progress), render whose turn it is.
-			// Hint: Use ternary inside of a template literal
-		// 3.3.2.2) If winner = 'T' (tie), render a tie message.
-		// 3.3.2.3) Else render a congratulatory message to which player has won.
-		// Hint (again): Use ternary inside a template literal 
-
-// 3.4) After completing this step, you should be able to manually change the values held in the board array in the initialization function and see the style of the corresponding square change on your page.
-
-// X 4) Define the required constants:
-
-	// X 4.1) Define the 8 possible winning combinations as an array of arrays.
-	  // X Each array will contain three indexes of the board that make a winner if they hold the same player value. 
-		// X If stuck: winningCombos array in the solution code. 
-
-// X 5) Wait for click on a square, call a handleClick function
-  // handleClick function will...
-  // X 5.1) Obtain the index of the square that was clicked by:
-	  // X 5.1.1) "Extracting" the index from square id assigned 
 		
-// X 5.2) If the board has a value at the index, return because that square is already taken.
-
-// X 5.3) If winner is not null, immediately return because the game is over.
-
-// 5.4) Update the board array at the index with the value of turn.
-
-// 5.5) Change the turn by multiplying turn by -1 (this flips a 1 to -1, and vice-versa).
-
-// X 5.6) Set the winner variable if there's a winner by calling a new function: getWinner.
-	// The getWinner function will...
-
-	// X 5.6.1) Methods to find out if there is a winner
-	// X 5.6.1.1) Loop through the each of the winning combination arrays defined.
-	// X 5.6.1.2) Total up the three board positions using the three indexes in the current combo.
-	// X 5.6.1.3) Convert the total to an absolute value (convert any negative total to positive).
-	// X 5.6.1.4) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
-
-	// X 5.6.3) Next, If there's no winner, check if there's a tie:
-  // X 5.6.4) Set the winner varible to "T" if there are no more nulls in the board array by returning the string "T".
-	// 5.6.5) Otherwise return null.
-
-// 5.7) All state has been updated, render the state to the page (step 3.3).
-
-// 6) Handle a player clicking the replay button:
-  // 6.1) Add a replay button to the HTML document
-  // 6.2) Store the new replay button element
-  // 6.3) Do steps 4.1 (initialize the state variables) and 4.2 (render).
-
-// AAU, I should see a tic tac toe board open on the page
-// AAU, I should be given directions to start the game
-// AAU, I should be able to select a square and put an X (or an O)
-// AAU, I should not be able to change a square's value
-// AAU, I should be able to complete the game
-// AAU, I should be made to feel like a winner, if I am the winner goddamnit
-// AAU, I should be told the game ends in a tie
-// AAU, I should be given the chance to play again
-
-// 
-
-// X when completed
-
-// X 1) Required Variables (state of the game) 
-// X    No variables need a value when defined 
+		render()
+}
 	
-// X 1.1) Array = squares;   
-// X 1.2) Turn Variable = whose turn it is;
-// X 1.3) Winner Variable = three game states:
-// X (not defined) a player won; a tie; game still in play;
 
-// 2) Cached Element References 
-// X 2.1) 9 elements = squares (each square a class name
-// X 2.2) element that displays the game status
-
-
-// 3) Upon loading, the app should:
-	// X 3.1) init();
-
-	// 3.2) function: init() state variables: (Array, Turn Variable, Winner Variable)
-		// X 3.2.1) Initialize Array to 9 nulls (empty squares) 
-		// **       9 elements "map" to each square: [0] = top-left square,    
-		// **       1] = top-middle square...[8] = bottom-right square)
-		// X 3.2.2) Initialize Turn to = (player 'X' = 1), (layer 	'O' = -1)
-		// X 3.2.3) Initialize Winner Variable = to null (no winner or tie)
-		// **       Winner Variable holds player value (1 or -1),
-		// **       if there's a winner; 'T' if there's a tie.
-		// X 3.2.4) render state variables, call a render function.
-
-
-// 
